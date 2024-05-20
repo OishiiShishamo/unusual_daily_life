2bca30fc24170eb2e907e07e01518a0b0ef6801a
Shishamo <shishamodesu@outlook.jp>
2024-05-20 17:12:35 +0900

    unko

--- lang_switch.js
+++ lang_switch.js
@@ -1,9 +1,11 @@
+//idがlang_switchのタグを言語変更ボタンとして動作させる
+
 if ( location.href.match(new RegExp('/ja/')) ){
     document.getElementById("lang_switch").innerHTML = "en"
-    document.getElementById("lang_switch").href = url.replace('/ja/', '/en/');
+    document.getElementById("lang_switch").setAttribute('href', location.href.replace('/ja/', '/en/'));
 }
 
 if ( location.href.match(new RegExp('/en/')) ){
     document.getElementById("lang_switch").innerHTML = "ja"
-    document.getElementById("lang_switch").href = url.replace('/en/', '/ja/');
+    document.getElementById("lang_switch").setAttribute('href', location.href.replace('/en/', '/ja/'));
 }
